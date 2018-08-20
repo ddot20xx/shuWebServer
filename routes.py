74afdb994a05b2aea7b6021695ae8e1e67e153af
ddot20xx <42403721+ddot20xx@users.noreply.github.com>
2018-08-20 17:01:05 +0800

    Update routes.py

--- routes.py
+++ routes.py
@@ -84,10 +84,10 @@ def route_static(request):
     """
     静态资源的处理函数, 读取图片并生成响应返回
     """
-    filename = request.query.get('file', 'doge.gif')
+    filename = request.query.get('file', 'doge.png')
     path = 'static/' + filename
     with open(path, 'rb') as f:
-        header = b'HTTP/1.1 200 OK\r\nContent-Type: image/gif\r\n\r\n'
+        header = b'HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n\r\n'
         img = header + f.read()
         return img
 
