ad194c14fd3940e82d3a123495b060834f554cd5
ddot20xx <42403721+ddot20xx@users.noreply.github.com>
2018-08-20 16:38:49 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,140 @@
+# coding: utf-8
+
+import socket
+import urllib.parse
+
+from utils import log
+
+from routes import route_static
+from routes import route_dict
+
+
+# 定义一个 class 用于保存请求的数据
+class Request(object):
+    def __init__(self):
+        self.method = 'GET'
+        self.path = ''
+        self.query = {}
+        self.body = ''
+        self.headers = {}
+        self.cookies = {}
+
+    def form(self):
+        body = urllib.parse.unquote(self.body)
+        args = body.split('&')
+        f = {}
+        for arg in args:
+            k, v = arg.split('=')
+            f[k] = v
+        return f
+
+    def add_cookies(self):
+        cookies = self.headers.get('cookie', '')
+        kvs = cookies.split('; ')
+        log('cookie', kvs)
+        for kv in kvs:
+            if '=' in kv:
+                k, v = kv.split('=')
+                self.cookies[k] = v
+
+    def add_headers(self, header):
+        for line in header:
+            k, v = line.split(': ', 1)
+            self.headers[k] = v
+        # set cookies
+        self.cookies = {}
+        self.add_cookies()
+
+
+request = Request()
+
+
+def error(request, code=404):
+    """
+    根据 code 返回不同的错误响应
+    目前只有 404
+    """    
+    e = {
+        404: 'HTTP/1.1 {} NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>'.format(code),
+    }
+    return e.get(code, '').encode('utf-8')
+
+
+def parsed_path(path):
+    """
+    将 path 解析为 path 和 query
+    """
+    index = path.find('?')
+    if index == -1:
+        return path, {}
+    else:
+        path, query_string = path.split('?', 1)
+        args = query_string.split('&')
+        query = {}
+        for arg in args:
+            k, v = arg.split('=')
+            query[k] = v
+        return path, query
+
+
+def response_for_path(path):
+    path, query = parsed_path(path)
+    request.path = path
+    request.query = query
+    log('path and query', path, query)
+    """
+    根据 path 调用相应的 route 处理函数
+    没有处理的 path 会返回 404
+    """
+    r = {
+        '/static': route_static,
+        # '/': route_index,
+        # '/login': route_login,
+        # '/messages': route_message,
+    }
+    r.update(route_dict)
+    response = r.get(path, error)
+    return response(request)
+
+
+def run(host='', port=3000):
+    """
+    启动服务器
+    """
+    # 初始化 socket  
+    log('start at', '{}:{}'.format(host, port))
+    with socket.socket() as s:
+        s.bind((host, port))
+        while True:
+            # 监听 接受 读取请求数据 解码成字符串
+            s.listen(5)
+            connection, address = s.accept()
+            r = connection.recv(1000)
+            r = r.decode('utf-8')            
+            # 因为 chrome 会发送空请求导致 split 得到空 list
+            # 所以这里判断一下防止程序崩溃
+            if len(r.split()) < 2:
+                continue
+            path = r.split()[1]
+            # 设置 request 的 method
+            request.method = r.split()[0]           
+            # 设置 request 的 headers
+            request.add_headers(r.split('\r\n\r\n', 1)[0].split('\r\n')[1:])
+            log('headers', request.headers)
+            # 把 body 放入 request 中
+            request.body = r.split('\r\n\r\n', 1)[1]
+            # 用 response_for_path 函数来得到 path 对应的响应内容
+            response = response_for_path(path)
+            # 把响应发送给客户端
+            connection.sendall(response)
+            # 处理完请求, 关闭连接
+            connection.close()
+
+
+if __name__ == '__main__':
+    # 生成配置并且运行程序
+    config = dict(
+        host='',
+        port=3000,
+    )
+    run(**config)
