fd40885f44ba2a2b35b584e6c3f8dae7625bd7df
ddot20xx <42403721+ddot20xx@users.noreply.github.com>
2018-08-22 19:16:25 +0800

    扩充utils.py
    1. 增加 jinja2 的模板渲染套路；
    2. 增加 error，response_with_headers，http_response，redirect 以及与前端交互的 json_response 函数。json_response 函数返回 json 格式的 body 数据。

--- utils.py
+++ utils.py
@@ -1,8 +1,83 @@
+from jinja2 import Environment, FileSystemLoader
+import os.path
 import time
+import json
 
 
-def log(*args, **kwargs):  
-    format = '%Y/%m/%d %H:%M:%S'
+def log(*args, **kwargs):
+    format = '%H:%M:%S'
     value = time.localtime(int(time.time()))
     dt = time.strftime(format, value)
-    print(dt, *args, **kwargs)
+    with open('log.gua.txt', 'a', encoding='utf-8') as f:
+        print(dt, *args, file=f, **kwargs)
+
+
+# __file__ 就是本文件的名字
+# 得到用于加载模板的目录
+path = '{}/templates/'.format(os.path.dirname(__file__))
+# 创建一个加载器, jinja2 会从这个目录中加载模板
+loader = FileSystemLoader(path)
+# 用加载器创建一个环境, 有了它才能读取模板文件
+env = Environment(loader=loader)
+
+
+def template(path, **kwargs):
+    """
+    本函数接受一个路径和一系列参数
+    读取模板并渲染返回
+    """
+    t = env.get_template(path)
+    return t.render(**kwargs)
+
+
+def response_with_headers(headers, status_code=200):
+    header = 'HTTP/1.1 {} OK\r\n'.format(status_code)
+    header += ''.join(['{}: {}\r\n'.format(k, v)
+                           for k, v in headers.items()])
+    return header
+
+
+def redirect(location):
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    headers['Location'] = location    
+    header = response_with_headers(headers, 302)
+    r = header + '\r\n' + ''
+    return r.encode(encoding='utf-8')
+
+
+def error(request, code=404):
+    """
+    根据 code 返回不同的错误响应    
+    """    
+    e = {
+        404: b'HTTP/1.1 404 NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>',
+    }
+    return e.get(code, b'')
+
+
+def http_response(body, headers=None):
+    """
+    headers 是可选的字典格式的 HTTP 头
+    """
+    header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n'
+    if headers is not None:
+        header += ''.join(['{}: {}\r\n'.format(k, v)
+                            for k, v in headers.items()])
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
+
+
+def json_response(data):
+    """
+    本函数返回 json 格式的 body 数据
+    前端的 ajax 函数就可以用 JSON.parse 解析出格式化的数据
+    """    
+    header = 'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n'
+    # json.dumps 用于把 list 或者 dict 转化为 json 格式的字符串
+    # ensure_ascii=False 可以正确处理中文
+    # indent=2 表示格式化缩进, 方便好看用的
+    body = json.dumps(data, ensure_ascii=False, indent=2)
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
