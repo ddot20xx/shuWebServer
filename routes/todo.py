8f1d50550295cbd5db160852eec4f491158fee23
ddot20xx <42403721+ddot20xx@users.noreply.github.com>
2018-08-22 19:35:02 +0800

    Create todo.py

---
+++ routes/todo.py
@@ -0,0 +1,25 @@
+from routes.session import session
+from utils import (
+    log,
+    redirect,
+    template,
+    http_response,
+)
+
+
+def main_index(request):
+    return redirect('/todo/index')
+
+
+def index(request):
+    """
+    主页的处理函数, 返回主页的响应
+    """
+    body = template('todo_index.html')
+    return http_response(body)
+
+
+route_dict = {
+    '/': main_index,
+    '/todo/index': index,
+}
